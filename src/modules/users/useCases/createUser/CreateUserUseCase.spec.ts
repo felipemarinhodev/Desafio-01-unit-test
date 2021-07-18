54539d64f3cc51d9e8d6da6424864104ad43e72c
Felipe Marinho <felipmarinho@gmail.com>
2021-07-18 08:40:50 -0300

    test: ensure not create a new user if already exists

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,19 +1,32 @@
+import { CreateUserError } from './CreateUserError';
 import { InMemoryUsersRepository } from '@modules/users/repositories/in-memory/InMemoryUsersRepository';
 import { CreateUserUseCase } from './CreateUserUseCase';
 
 describe('Create User Use Case', () => {
   it('should be able create a new user', async () => {
-    const inMemoryUsersRepository = new InMemoryUsersRepository()
-    const useCase = new CreateUserUseCase(inMemoryUsersRepository)
+    const { useCase, userMock } = makeSut();
 
-    const user = {
-      name: 'Fulano Silva',
-      email: 'fulanosilva@email.com',
-      password: '123456'
-    }
-
-    const result = await useCase.execute(user)
+    const result = await useCase.execute(userMock)
     expect(result).toHaveProperty('id')
   })
 
+  it('should not be able to create a new user if already exists', async () => {
+    const { useCase, userMock } = makeSut()
+
+    await useCase.execute(userMock);
+    expect(async () => {
+      await useCase.execute(userMock);
+    }).rejects.toBeInstanceOf(CreateUserError)
+  });
 })
+function makeSut() {
+  const inMemoryUsersRepository = new InMemoryUsersRepository();
+  const useCase = new CreateUserUseCase(inMemoryUsersRepository);
+  const userMock = {
+    name: 'Fulano Silva',
+    email: 'fulanosilva@email.com',
+    password: '123456'
+  }
+  return { useCase, userMock };
+}
+
