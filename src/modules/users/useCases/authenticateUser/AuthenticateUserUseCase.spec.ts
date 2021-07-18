b6263d1ead9e41c7f47968de7d67064cd6deb90d
Felipe Marinho <felipmarinho@gmail.com>
2021-07-18 10:34:08 -0300

    Add test

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,49 @@
+import "reflect-metadata";
+import { CreateUserUseCase } from "./../createUser/CreateUserUseCase";
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from './IncorrectEmailOrPasswordError';
+
+let userRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+describe("Authenticate User Use Case", () => {
+
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(userRepository);
+    authenticateUserUseCase = new AuthenticateUserUseCase(userRepository);
+  });
+
+  async function createUser(): Promise<ICreateUserDTO> {
+    const userDTO: ICreateUserDTO = {
+      email: "fulano@email.com",
+      password: "123456",
+      name: "Fulano da Silva",
+    };
+    await createUserUseCase.execute(userDTO);
+    return userDTO;
+  }
+
+  it("should be able sign in correctly", async () => {
+    const user = await createUser();
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able sign in with incorrect email", async () => {
+    const user = await createUser();
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: 'incorrect@email.com',
+        password: user.password,
+      })
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  });
+
+});
